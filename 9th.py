e7c1d09d13a71b83265765972f1665a72ce01881
Veronika Chagina <90192566+Veronika-Chagina@users.noreply.github.com>
2022-06-27 09:03:51 +0300

    Update 9th.py

--- 9th.py
+++ 9th.py
@@ -1,17 +1,16 @@
-def prefix_function(S):
-    p = [0]*(len(S)+1)
+def prefix_function(text):
+    p = [0 for a in range(len(text) - 1)]
     i, j = 1, 0
-    while i < len(S):
-        if S[i] == S[j]:
-            p[i+1] = j + 1
+    while i < len(text)-1:
+        if text[i] == text[j]:
+            p[i] = j + 1
             i += 1
-            j += 1 
+            j += 1
+        elif j > 0:
+            j = p[j-1]
         else:
-            if j > 0:
-                j = p[j]
-            else:
-                p[i+1] = 0
-                i += 1
+            p[i] = 0
+            i += 1
     return p
 def decomposition(S):
     p = prefix_function(S)
